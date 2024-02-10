bebb78dd5484794839d005394aaec441d13d9b31
JoJo <joehearthstone@gmail.com>
2024-02-10 12:28:17 -0500

    basic setup

---
+++ calcsub/main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
+import kivy
+
+class MyRoot(BoxLayout):
+
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+class JoJoCalc(App):
+
+    def build(self):
+        return MyRoot()
