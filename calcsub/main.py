db7183d87a4885a2e7ed6e54dcd534337a84e4a8
JoJo <joehearthstone@gmail.com>
2024-02-10 12:59:45 -0500

    hmmm didnt work

--- calcsub/main.py
+++ calcsub/main.py
@@ -8,6 +8,9 @@ class MyRoot(BoxLayout):
     def __init__(self):
         super(MyRoot, self).__init__()
 
+    def calc_symbol(self, symbol):
+        self.calc_field.text += symbol
+
 class JoJoCalc(App):
 
     def build(self):
